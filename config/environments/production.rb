53e8473f32cdee9da070efdb3ae335c9f31e4ea4
Julia <julia.s.78@outlookom>
2015-10-05 10:25:41 -0400

    added config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif) to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
